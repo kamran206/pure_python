3de6ed290197c4af1b7914a7a3b37938c7a7ce28
Mohammad kamran khan <71761323+kamran206@users.noreply.github.com>
2023-12-22 11:49:59 +0500

    Add files via upload

---
+++ basic_python.py
@@ -0,0 +1,15 @@
+#print command in python
+print('Kamran is an Artificial Intelligence Engineer')
+print(2+3)
+print('Name', 'Is', 'James')
+
+#For loop1
+for i in range(5):
+    print(i)
+
+#While loop
+i = 0
+while i<10:
+    print(i)
+    i += 1
+    
